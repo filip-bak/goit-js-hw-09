89469fd34acf3f35df1da93b31df8bf994ef481a
Filip Bączkowski <filo.b@wp.pl>
2023-04-26 11:44:19 +0200

    feat: first task completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const bodyEl = document.body;
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+stopBtn.disabled = true;
+
+let timerId = null;
+
+function changeBgColor() {
+  return (document.body.style.backgroundColor = getRandomHexColor());
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  changeBgColor();
+  timerId = setInterval(changeBgColor, 1000);
+
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
