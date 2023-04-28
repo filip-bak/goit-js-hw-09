effaed7b541a46ae445a537374931540bc73b78b
Filip Bączkowski <filo.b@wp.pl>
2023-04-28 22:15:37 +0200

    feat: completed 80% of third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,57 @@
+import Notiflix from 'notiflix';
+Notiflix.Notify.init({
+  pauseOnHover: false,
+});
+
+const formEl = document.querySelector('.form');
+
+function getinputValues(e) {
+  const {
+    elements: {
+      delay: { value: delayValue },
+      step: { value: stepValue },
+      amount: { value: amountValue },
+    },
+  } = formEl;
+
+  return {
+    delay: +delayValue,
+    step: +stepValue,
+    amount: +amountValue,
+  };
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  const data = {
+    position,
+    delay,
+  };
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      shouldResolve ? resolve(data) : reject(data);
+    }, delay);
+  });
 }
+// Need validation on (number < 0)
+// formEl.addEventListener("change",() => {
+
+// })
+formEl.addEventListener('submit', e => {
+  e.preventDefault();
+  const input = getinputValues();
+
+  for (let i = 1; i <= input.amount; i++) {
+    createPromise(i, input.delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
+
+    input.delay += input.step;
+  }
+});
