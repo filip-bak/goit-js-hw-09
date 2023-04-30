4e264858530147b692334bb9d9e8f048c8caee5d
Filip Bączkowski <filo.b@wp.pl>
2023-04-30 04:22:55 +0200

    feat: "clean code"

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,25 +6,45 @@ stopBtn.disabled = true;
 
 let timerId = null;
 
-function changeBgColor() {
-  return (document.body.style.backgroundColor = getRandomHexColor());
+//                  😔 TO BIG FUNCTION! 😔
+// function switchButtonsQ({ firstBtnEl, secondBtnEl, disableFirstBtn = true }) {
+//   let disableSecondBtn = false;
+
+//   firstBtnEl.disabled = disableFirstBtn;
+//   secondBtnEl.disabled = disableSecondBtn;
+
+//   if (disableFirstBtn === false) {
+//     disableSecondBtn = true;
+//     firstBtnEl.disabled = disableFirstBtn;
+//     secondBtnEl.disabled = disableSecondBtn;
+//     return;
+//   }
+// }
+
+function buttonSwitch({ trueOrder = true } = {}) {
+  startBtn.disabled = trueOrder;
+  trueOrder = !trueOrder;
+  stopBtn.disabled = trueOrder;
 }
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+function changeBgColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
 startBtn.addEventListener('click', () => {
   changeBgColor();
+
   timerId = setInterval(changeBgColor, 1000);
 
-  startBtn.disabled = true;
-  stopBtn.disabled = false;
+  buttonSwitch();
 });
 
 stopBtn.addEventListener('click', () => {
   clearInterval(timerId);
 
-  startBtn.disabled = false;
-  stopBtn.disabled = true;
+  buttonSwitch({ trueOrder: false });
 });
